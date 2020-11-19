1ca047eaa9dc84bbc35a6c23d88863ee72d417f6
kaizeng <1008611>
2020-11-19 13:57:16 +0800

    update database connect

--- src/main/java/com/whgc/springbootssm1/Springbootssm1Application.java
+++ src/main/java/com/whgc/springbootssm1/Springbootssm1Application.java
@@ -3,7 +3,6 @@ package com.whgc.springbootssm1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.whgc.springbootssm1.dao.mapper")
@@ -13,10 +12,4 @@ public class Springbootssm1Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Springbootssm1Application.class, args);
     }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-
-        return builder.sources(Springbootssm1Application.class);
-    }
-
 }
