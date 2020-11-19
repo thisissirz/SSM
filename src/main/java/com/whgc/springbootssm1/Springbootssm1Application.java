933e848475bdb26b5de950e7f7960ed3dbd77695
kaizeng <1008611>
2020-11-19 14:34:42 +0800

    1

--- src/main/java/com/whgc/springbootssm1/Springbootssm1Application.java
+++ src/main/java/com/whgc/springbootssm1/Springbootssm1Application.java
@@ -12,4 +12,12 @@ public class Springbootssm1Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Springbootssm1Application.class, args);
     }
+
+/*
+    tomcat package need
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Springbootssm1Application.class);
+    }
+*/
 }
